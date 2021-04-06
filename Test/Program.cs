2b01abcbfbc855310cd1db091925e338098d4f6b
Тарас Крупко <121-19-6-krupko@knute.edu.ua>
2021-04-06 20:18:11 +0300

    Добавьте файлы проекта.

---
+++ Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Test
+{
+    class Program
+    {
+        static Mutex mutexObj = new Mutex();
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread t = new Thread(Count);
+                t.Name = $"Поток: {i}";
+                t.Start();
+            }
+        }
+
+        public static void Count()
+        {
+            mutexObj.WaitOne();
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name}: {i}");
+                Thread.Sleep(100);
+            }
+            mutexObj.ReleaseMutex();
+        }
+    }
+}
